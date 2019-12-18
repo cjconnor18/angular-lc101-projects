2feae4ad62a0e825a945498595b061d958de9c9c
Carol Connor <cjallen1988@gmail.com>
2019-12-17 19:13:32 -0600

    first commit exercise 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://images-na.ssl-images-amazon.com/images/I/31Y3Z64XZQL.jpg';
+  image3 = 'https://www.televisionpost.com/wp-content/uploads/2019/04/OceanS-8-inside.jpg';
 
   constructor() { }
 
